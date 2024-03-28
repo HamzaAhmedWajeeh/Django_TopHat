114d136ad566145b68114dbb111255c2534f3c9d
hamza-wajeeh <hamza.wajeeh@inseyab.com>
2024-03-28 13:01:28 +0500

    Fixing payment intent errors

--- app/payments/views.py
+++ app/payments/views.py
@@ -145,10 +145,9 @@ class PaymentIntent(GenericAPIView):
                     customer=customer,
                     currency='aud',
                     amount=int(total_amount * 100),
-                    confirm=True,
-                    return_url="http://localhost:9001/",
+                    # return_url="http://localhost:9001/",
                     receipt_email=user.email,
-                    automatic_payment_methods={"enabled": True},
+                    # automatic_payment_methods={"enabled": True},
                 )
 
         return Response({
