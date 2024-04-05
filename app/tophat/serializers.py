477ce8395ae87908a3ffda2d09c900365e83f6a2
hamza-wajeeh <hamza.wajeeh@inseyab.com>
2024-04-05 17:21:17 +0500

    added order history api and now testing cart get

--- app/tophat/serializers.py
+++ app/tophat/serializers.py
@@ -262,14 +262,12 @@ class CartSerializer(serializers.ModelSerializer):
         return obj.item.image.url if obj.item and obj.item.image else None
 
     def get_extras_info(self, obj):
-        extras_ids = obj.extras.split(', ') if obj.extras else []
-        print("Extras IDs:", extras_ids)
+        extras_ids = obj.extras.split(',') if obj.extras else []
         extras_info = Extras.objects.filter(pk__in=extras_ids).values('name', 'price')
         return list(extras_info)
 
     def get_kitchen_notes_info(self, obj):
-        kitchen_notes_ids = obj.kitchen_notes.split(', ') if obj.kitchen_notes else []
-        print("Kitchen Notes IDs:", kitchen_notes_ids)
+        kitchen_notes_ids = obj.kitchen_notes.split(',') if obj.kitchen_notes else []
         kitchen_notes_info = KitchenNotes.objects.filter(pk__in=kitchen_notes_ids).values('name', 'price')
         return list(kitchen_notes_info)
 
