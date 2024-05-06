f38e1224af87ac063ff711a1e97c28cd99b427f2
hamza-wajeeh <hamza.wajeeh@inseyab.com>
2024-05-06 15:42:24 +0500

    fixed settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -174,6 +174,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
     "http://162.0.213.111:9011",
+    "http://tophatcoffee.co",
+    "https://tophatcoffee.co",
 
 ]
 
